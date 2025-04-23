6063b715562f48129717105206cccfd72268de3c
Joshua Marandi <134075135+joshuaMarandi@users.noreply.github.com>
2025-04-23 20:55:54 +0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
  
-
   <!-- Your CSS and other head elements here -->
   <style>
     /* Header styles */
